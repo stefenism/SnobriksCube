2ab0079e12288aaf2c60519275a79cd38a8f672c
Zack-Laptop <zack.gilbertson@gmail.com>
2019-01-26 12:35:35 -0600

    -upgrade unity add cube scripts

---
+++ Assets/Scripts/Cube/CubeScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 center = new Vector3(5,15,15);
+        transform.RotateAround(center, Vector3.up, 20 * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/Cube/MouseScript.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+    
+    Ray ray;
+    RaycastHit hit;
+    
+    bool holdingSelector = false;
+
+    //mouseVars
+    float lastMouseX=0;
+    float lastMouseY=0;
+    float totalMouseX=0;
+    float totalMouseY=0;
+    void Update()
+    {
+        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out hit))
+        {
+            if (Input.GetMouseButtonDown(0))
+            { 
+                if (hit.collider.tag == "Selector")
+                {
+                    holdingSelector=true;
+                    totalMouseX=0;
+                    totalMouseY=0;
+                    print(hit.collider.name);
+                }
+            }
+        }
+        if (Input.GetMouseButtonUp(0)){
+            holdingSelector=false;
+        }
+        if(holdingSelector){
+            totalMouseX+=Input.mousePosition.x-lastMouseX;
+            totalMouseY+=Input.mousePosition.y-lastMouseY;
+            if(abs(totalMouseX)>20){
+                print("Mouse Big");
+            }
+        }
+
+        lastMouseX=Input.mousePosition.x;
+        lastMouseY=Input.mousePosition.y;
+    }
+
+
+}
