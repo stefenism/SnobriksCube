e58d5d9ed7831a719329e80c0c2f38660b5f5a0a
Zack-Laptop <zack.gilbertson@gmail.com>
2019-01-26 19:22:35 -0600

    -added camera script

---
+++ Assets/Scripts/Camera/CameraController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject playerCharacter;
+    Vector2 velocity;
+
+    public float smoothTimeY;
+    public float smoothTimeX;
+    // Update is called once per frame
+    void Update()
+    {
+        //transform.position = playerCharacter.transform.position+new Vector3(0,0,-1);
+        float posX = Mathf.SmoothDamp(transform.position.x,playerCharacter.transform.position.x,ref velocity.x,smoothTimeX);
+        float posY = Mathf.SmoothDamp(transform.position.y,playerCharacter.transform.position.y,ref velocity.y,smoothTimeY);
+        transform.position = new Vector3(posX,posY,-2);
+    }
+}
